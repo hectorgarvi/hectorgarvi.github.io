b8863bdeb0666bbedb1d679b1507cd3d77385aa5
hectorgarvi <100135701+hectorgarvi@users.noreply.github.com>
2022-03-21 14:35:53 +0100

    Add files via upload

---
+++ Formularios/Ejercicios/precio/ejemploPost2.php
@@ -0,0 +1,18 @@
+<?php
+
+$precio1 = $_POST['precio1'];
+$precio2 = $_POST['precio2'];
+$precio3 = $_POST['precio3'];
+
+
+$media = ($precio1 + $precio2 + $precio3) / 3;
+
+echo "<br/> &nbsp; DATOS RECIBIDOS";
+
+echo "<br/> &nbsp; Precio producto establecimiento 1: " . $precio1 . " euros";
+echo "<br/> &nbsp; Precio producto establecimiento 2: " . $precio2 . " euros";
+echo "<br/> &nbsp; Precio producto establecimiento 3: " . $precio3 . " euros";
+
+echo "<br/> &nbsp; El precio medio del producto es " . $media . " euros";
+
+?>
